46c6cfe7a946139ce32a859b31f113f4b2c0a952
carlosLapa <carlosferreiralapa@gmail.com>
2022-09-21 17:55:41 +0100

    API call working

--- frontweb/src/components/ResultCard/index.tsx
+++ frontweb/src/components/ResultCard/index.tsx
@@ -1,11 +1,18 @@
 import './styles.css';
 
-const ResultCard = () => {
-  return (
-    <div className="result-container">
-      <h1> Resultados</h1>
-    </div>
-  );
-};
+type Props = {
+    title: string;
+    description: string;
+}
+
+const ResultCard = ( { title, description} : Props) => {
+
+    return (
+        <div className="result-container">
+            <h3 className="result-title">{title}</h3>
+            <p className="result-description">{description}</p>
+        </div>
+    );
+}
 
 export default ResultCard;
--- frontweb/src/pages/CepSearch/index.tsx
+++ frontweb/src/pages/CepSearch/index.tsx
@@ -1,17 +1,63 @@
+import axios from 'axios';
+import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
 import './styles.css';
 
+type formData = {
+  gitProfile: string;
+};
+
+type Profile = {
+  login: string,
+  url: string,
+  avatar_url: string,
+  followers: number,
+  location: string,
+  name: string
+};
+
 const CepSearch = () => {
+  const [profile, setProfile] = useState<Profile>();
+
+  const [formData, setFormData] = useState<formData>({
+    gitProfile: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    axios.get(`https://api.github.com/users/${formData.gitProfile}`)
+    .then((response) => {
+      setProfile(response.data);
+      console.log(response.data);
+    })
+    .catch((error) => {
+      setProfile(undefined);
+      console.log(error);
+    })
+
+  };
+
   return (
     <div className="cep-search-container">
       <div className="container search-container">
         <h1 className="text-title">Encontre um perfil Github</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="gitProfile"
+              value={formData.gitProfile}
               className="search-input"
               placeholder="Usuário Github"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Encontrar
@@ -21,6 +67,13 @@ const CepSearch = () => {
       </div>
       <div className="container cep-search-result">
         <h1>Teste</h1>
+      {profile && (
+          <>
+            <ResultCard title="Perfil" description={profile.url} />
+            <ResultCard title="Nome" description={profile.name} />
+          </>
+        )}
+
       </div>
     </div>
   );
